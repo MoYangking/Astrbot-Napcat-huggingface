5fbc653525df9749223bad4453a7953bafc5c7aa
moyang <3034065341@qq.com>
2025-11-26 17:22:29 +0800

    Revert "feat(docker): add dbus-x11 and GPU rendering flags for AppImage execution"
    This reverts commit 2d6b9286576ca299fcc233ffb2d3220b59bd6011.

--- scripts/run-napcat.sh
+++ scripts/run-napcat.sh
@@ -42,8 +42,8 @@ EOF
 fi
 
 if [ -x /home/user/QQ.AppImage ]; then
-  exec dbus-launch $PROXY_PREFIX /home/user/QQ.AppImage --appimage-extract-and-run --no-sandbox --disable-gpu --disable-software-rasterizer ${NAPCAT_FLAGS:-}
+  exec $PROXY_PREFIX /home/user/QQ.AppImage --appimage-extract-and-run --no-sandbox ${NAPCAT_FLAGS:-}
 fi
 
 # Fallback to extracted AppRun
-exec dbus-launch $PROXY_PREFIX /home/user/napcat/AppRun --no-sandbox --disable-gpu --disable-software-rasterizer ${NAPCAT_FLAGS:-}
+exec $PROXY_PREFIX /home/user/napcat/AppRun --no-sandbox ${NAPCAT_FLAGS:-}
